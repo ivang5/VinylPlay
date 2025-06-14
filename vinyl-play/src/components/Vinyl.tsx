4fbaef6215eb8969cdddb14c70ad4232c2a3002f
ivang5 <ivanprogaborov@gmail.com>
2025-06-14 19:48:45 +0200

    Adding Vinyl component

---
+++ vinyl-play/src/components/Vinyl.tsx
@@ -0,0 +1,9 @@
+export const Vinyl = () => (
+  <div className="vinyl-pattern size-140 rounded-full relative border-8 border-black">
+    <div className="absolute inset-1/2 size-68.5 -translate-1/2 bg-black rounded-full"></div>
+    <div className="absolute inset-1/2 size-66 -translate-1/2 bg-black border-2 border-[#1a1a1a] rounded-full"></div>
+    <div className="absolute inset-1/2 size-62 -translate-1/2 bg-black border-2 border-[#1a1a1a] rounded-full"></div>
+    <div className="absolute inset-1/2 size-56 -translate-1/2 bg-red-600 rounded-full"></div>
+    <div className="absolute inset-1/2 size-3 -translate-1/2 bg-gray-300 rounded-full"></div>
+  </div>
+);
