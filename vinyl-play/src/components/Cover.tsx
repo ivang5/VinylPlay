272d51ca24de062d6a27b4048f002839e3a526ec
ivang5 <ivanprogaborov@gmail.com>
2025-09-01 21:33:38 +0200

    Making small changes to cover

--- vinyl-play/src/components/Cover.tsx
+++ vinyl-play/src/components/Cover.tsx
@@ -4,12 +4,14 @@ type CoverPropType = {
 
 export const Cover = ({ image }: CoverPropType) => {
   return (
-    <div className="relative size-140 rounded-sm overflow-hidden shadow-lg">
-      <img
-        className="w-full h-full object-cover"
-        src={image}
-        alt="Album Cover"
-      />
+    <div className="relative size-140 rounded-sm overflow-hidden shadow-2xl">
+      {image ? (
+        <img className="size-full object-cover" src={image} alt="Album Cover" />
+      ) : (
+        <div className="size-full flex items-center justify-center bg-gray-200">
+          <h3 className="font-bold text-5xl opacity-80 ">Unknown...</h3>
+        </div>
+      )}
       <div className="absolute inset-0 pointer-events-none bg-[url('/textures/crumpled-paper-2.png')] opacity-20 mix-blend-multiply"></div>
       <div className="absolute inset-0 pointer-events-none bg-[url('/textures/crumpled-paper.png')] mix-blend-multiply"></div>
     </div>
