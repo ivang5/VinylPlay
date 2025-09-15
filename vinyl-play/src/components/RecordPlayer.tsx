8f830125dbdf0e0aca04a95ebb715e1c1884681f
ivang5 <ivanprogaborov@gmail.com>
2025-09-15 21:10:47 +0200

    Small record player update

--- vinyl-play/src/components/RecordPlayer.tsx
+++ vinyl-play/src/components/RecordPlayer.tsx
@@ -1,7 +1,10 @@
 export const RecordPlayer = () => {
   return (
-    <div className="bg-gray-600 relative w-5xl h-[680px] rounded-sm">
-      <div className="rounded-full absolute inset-y-10 left-10 w-[600px] bg-black"></div>
+    <div className="bg-gray-600 relative w-5xl h-[712px] rounded-sm">
+      <div className="rounded-full absolute inset-y-10 left-10 w-[632px] bg-[#D8D8D8]"></div>
+      <div className="rounded-full absolute inset-y-[43px] left-[43px] w-[626px] bg-[#0F0F10]"></div>
+      <div className="rounded-full absolute inset-y-[76px] left-[76px] w-[560px] bg-black"></div>
+      <div className="rounded-full absolute inset-y-[351px] left-[351px] w-[10px] bg-[#DFDFDF]"></div>
     </div>
   );
 };
