e3f24da4f3fc1d968f8d080d3ee029ca4e30a950
ivang5 <ivanprogaborov@gmail.com>
2025-09-16 20:52:48 +0200

    Update Record Player component and add Platter to it

--- vinyl-play/src/components/RecordPlayer/RecordPlayer.tsx
+++ vinyl-play/src/components/RecordPlayer/RecordPlayer.tsx
@@ -1,8 +1,12 @@
+import { Platter } from "./Platter";
+
 export const RecordPlayer = () => {
   return (
-    <div className="bg-gray-600 relative w-5xl h-[712px] rounded-sm">
+    <div className="bg-[#3C4544] relative w-6xl h-[712px] rounded-sm">
+      <div className="absolute inset-0 bg-[url('/textures/noise.png')]"></div>
+      <div className="rounded-full absolute inset-y-[35px] left-[35px] w-[642px] bg-black"></div>
       <div className="rounded-full absolute inset-y-10 left-10 w-[632px] bg-[#D8D8D8]"></div>
-      <div className="rounded-full absolute inset-y-[43px] left-[43px] w-[626px] bg-[#0F0F10]"></div>
+      <Platter />
       <div className="rounded-full absolute inset-y-[76px] left-[76px] w-[560px] bg-black"></div>
       <div className="rounded-full absolute inset-y-[351px] left-[351px] w-[10px] bg-[#DFDFDF]"></div>
     </div>
