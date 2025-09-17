abf2e58a50475132ceb912101de0d5671abc5c7e
ivang5 <ivanprogaborov@gmail.com>
2025-09-17 21:00:38 +0200

    Adding Tonearm to RecordPlayer

--- vinyl-play/src/components/RecordPlayer/RecordPlayer.tsx
+++ vinyl-play/src/components/RecordPlayer/RecordPlayer.tsx
@@ -1,4 +1,5 @@
 import { Platter } from "./Platter";
+import { Tonearm } from "./Tonearm";
 
 type RecordPlayerPropType = {
   shouldSpin?: boolean;
@@ -7,11 +8,14 @@ type RecordPlayerPropType = {
 export const RecordPlayer = ({ shouldSpin = false }: RecordPlayerPropType) => {
   return (
     <div className="bg-[#3C4544] relative w-6xl h-[712px] rounded-sm">
-      <div className="absolute inset-0 bg-[url('/textures/noise.png')]"></div>
-      <div className="rounded-full absolute inset-y-[35px] left-[35px] w-[642px] bg-black"></div>
-      <Platter shouldSpin={shouldSpin} />
-      <div className="rounded-full absolute inset-y-[76px] left-[76px] w-[560px] bg-black"></div>
-      <div className="rounded-full absolute inset-y-[351px] left-[351px] w-[10px] bg-[#DFDFDF]"></div>
+      <div className="absolute inset-0 z-10">
+        <div className="absolute inset-0 bg-[url('/textures/noise.png')]"></div>
+        <div className="rounded-full absolute inset-y-[35px] left-[35px] w-[642px] bg-black"></div>
+        <Platter shouldSpin={shouldSpin} />
+        <div className="rounded-full absolute inset-y-[76px] left-[76px] w-[560px] bg-black"></div>
+        <div className="rounded-full absolute inset-y-[351px] left-[351px] w-[10px] bg-[#DFDFDF]"></div>
+      </div>
+      <Tonearm />
     </div>
   );
 };
