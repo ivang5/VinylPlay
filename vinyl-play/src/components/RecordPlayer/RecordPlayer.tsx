4106c8c67ba1bde97e173973fdc2fef9b2bf4303
ivang5 <ivanprogaborov@gmail.com>
2025-09-18 20:48:58 +0200

    Small Tonearm updates

--- vinyl-play/src/components/RecordPlayer/RecordPlayer.tsx
+++ vinyl-play/src/components/RecordPlayer/RecordPlayer.tsx
@@ -8,8 +8,8 @@ type RecordPlayerPropType = {
 export const RecordPlayer = ({ shouldSpin = false }: RecordPlayerPropType) => {
   return (
     <div className="bg-[#3C4544] relative w-6xl h-[712px] rounded-sm">
+      <div className="absolute inset-0 bg-[url('/textures/noise.png')]"></div>
       <div className="absolute inset-0 z-10">
-        <div className="absolute inset-0 bg-[url('/textures/noise.png')]"></div>
         <div className="rounded-full absolute inset-y-[35px] left-[35px] w-[642px] bg-black"></div>
         <Platter shouldSpin={shouldSpin} />
         <div className="rounded-full absolute inset-y-[76px] left-[76px] w-[560px] bg-black"></div>
--- vinyl-play/src/components/RecordPlayer/Tonearm.tsx
+++ vinyl-play/src/components/RecordPlayer/Tonearm.tsx
@@ -1,19 +1,44 @@
 export const Tonearm = () => {
+  const lines = Array.from({ length: 24 });
+  const radius = 88;
+
   return (
-    <div className="absolute top-[70px] right-[280px] rounded-full bg-[#111111] size-64 z-0">
+    <div className="absolute top-[70px] right-[280px] rounded-full bg-[#171717] size-64 z-0">
       <svg viewBox="0 0 200 200" className="absolute inset-0 w-full h-full">
         <circle
           cx="100"
           cy="100"
           r="77"
           fill="none"
-          stroke="black"
+          stroke="#0b0b0b"
           strokeWidth="30"
           strokeLinecap="round"
           strokeDasharray="300 145"
           strokeDashoffset="150"
         />
       </svg>
+      <div className="absolute inset-[41px] size-44 rounded-full bg-black">
+        {lines.map((_, i) => {
+          const angle = i * (360 / lines.length);
+          const isLong = i % 2 === 0;
+          return (
+            <div
+              key={i}
+              className="absolute w-px bg-white"
+              style={{
+                height: isLong ? radius * 0.98 : radius * 0.93,
+                left: "50%",
+                bottom: "50%",
+                transform: `translateX(-50%) rotate(${angle}deg)`,
+                transformOrigin: "center bottom",
+              }}
+            />
+          );
+        })}
+      </div>
+      <div className="absolute inset-[51px] size-[156px] rounded-full bg-linear-[45deg,#999_5%,#fff_10%,#ccc_30%,#ddd_50%,#ccc_70%,#fff_80%,#999_95%]">
+        <div className="absolute opacity-50 inset-0 bg-[url('/textures/noise.png')]"></div>
+      </div>
     </div>
   );
 };
